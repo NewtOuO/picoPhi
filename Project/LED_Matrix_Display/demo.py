207582a6f623943e088896719060c091c6f67331
Newt <hsincheng.1130@gmail.com>
2024-01-06 14:00:37 +0000

    update: code optimization

--- Project/LED_Matrix_Display/demo.py
+++ Project/LED_Matrix_Display/demo.py
@@ -1,58 +1,74 @@
 """
 Author: Newt
 """
-from ws2812b import PixelRGB, PixelBool
+from ws2812b import Pixel
 from pixelation import ImagePixelator, TextPixelator
-import time
+from time import sleep
+
+pxsize = (16, 16)
+px = Pixel(pxsize)
+
+img_pxr = ImagePixelator(pxsize)
+img0 = img_pxr.image_px("img_0.png")
+img1 = img_pxr.image_px("img_1.png")
+img2 = img_pxr.image_px("img_2.png")
+
+txt_pxr = TextPixelator((32, 16), 12, "NotoSansTC.ttf")
+txt = txt_pxr.text_px("Newt")
+
+
+def demo_1() -> None:
+    for y in range(-16, 16):
+        px.draw(img0, (0, y))
+        px.display()
+        px.clear()
+    px.display()
+
+
+def demo_2() -> None:
+    for x in range(-16, 16):
+        px.draw(txt, (x, 0))
+        px.display()
+        px.clear()
+    px.display()
+
+
+def demo_3() -> None:
+    for _ in range(3):
+        px.draw(img0, (0, 0))
+        px.display()
+        px.clear()
+        sleep(0.2)
+        px.draw(img1, (0, 0))
+        px.display()
+        px.clear()
+        sleep(0.2)
+        px.draw(img0, (0, 0))
+        px.display()
+        px.clear()
+        sleep(0.2)
+        px.draw(img2, (0, 0))
+        px.display()
+        px.clear()
+        sleep(0.2)
+    px.display()
+
+
+def demo_4() -> None:
+    for _ in range(3):
+        for x in range(-20, 20):
+            px.draw(img0)
+            px.draw(txt, (x, 0))
+            px.display()
+            px.clear()
+    px.display()
 
 
 def main() -> None:
-    img = ImagePixelator((16, 16))
-    txt = TextPixelator((32, 16), 12, "NotoSansTC.ttf")
-
-    pxrgb = PixelRGB((16, 16))
-    img0 = img.image_px("img_0.png")
-    img1 = img.image_px("img_1.png")
-    img2 = img.image_px("img_2.png")
-
-    pxbool = PixelBool((16, 16))
-    txt = txt.text_px("Newt")
-
-    # Demo 1
-    for a in range(-15, 16):
-        pxrgb.draw(img0, (0, a))
-        pxrgb.display()
-        pxrgb.clear()
-        time.sleep(0.05)
-    pxrgb.display()
-
-    # Demo 2
-    for b in range(-32, 16):
-        pxbool.draw(txt, (b, 0))
-        pxbool.display((32, 32, 0), (0, 0, 16))
-        pxbool.clear()
-        time.sleep(0.05)
-    pxbool.display((0, 0, 0), (0, 0, 0))
-
-    # Demo 3
-    for c in range(5):
-        pxrgb.draw(img0, (0, 0))
-        pxrgb.display()
-        pxrgb.clear()
-        time.sleep(0.2)
-        pxrgb.draw(img1, (0, 0))
-        pxrgb.display()
-        pxrgb.clear()
-        time.sleep(0.2)
-        pxrgb.draw(img0, (0, 0))
-        pxrgb.display()
-        pxrgb.clear()
-        time.sleep(0.2)
-        pxrgb.draw(img2, (0, 0))
-        pxrgb.display()
-        pxrgb.clear()
-        time.sleep(0.2)
-    pxrgb.display()
+    demo_1()
+    demo_2()
+    demo_3()
+    demo_4()
 
 
 if __name__ == "__main__":
